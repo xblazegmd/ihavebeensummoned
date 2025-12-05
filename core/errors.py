c9ff8a91d83d5019e6a50f49a0c2c4c4bbd95607
xblazegmd <santiagopalaciocolina7@gmail.com>
2025-12-05 13:04:41 -0500

    Added more errors I can use

--- core/errors.py
+++ core/errors.py
@@ -7,8 +7,12 @@ class IHBSError(Exception):
     def __init__(self, *args):
         super().__init__(*args)
 
+# ---------------------
+#     MISC ERRORS
+# ---------------------
+
 # In case _____ is not found
-class NotFoundError(Exception):
+class NotFoundError(IHBSError):
     """
     In case _____ is not found
     """
@@ -24,15 +28,20 @@ class OperationFailedError(Exception):
         super().__init__(*args)
 
 # The attempted operation is on cooldown
-class CooldownError(Exception):
+# The remaining time will be specified (in seconds)
+class CooldownError(IHBSError):
     """
     The attempted operation is on cooldown
+
+    The remaining time will be specified (in seconds)
     """
     def __init__(self, *args, remaining: int):
         super().__init__(*args)
         self.remaining = remaining
 
-# External API errors
+# ---------------------
+#  EXTERNAL API ERRORS
+# ---------------------
 
 # An unexpected error when connecting to the Boomlings endpoint
 class BoomlingsError(IHBSError):
@@ -42,10 +51,18 @@ class BoomlingsError(IHBSError):
     ...
 
 # Request to the API returned a 403 FORBIDDEN error
+# This usually happens when the request to the Boomlings endpoint is invalid (no www subdomain, non-empty User-Agent)
 class HTTPForbiddenError(IHBSError):
     """
     Request to the API returned a 403 FORBIDDEN error
 
     This usually happens when the request to the Boomlings endpoint is invalid (no www subdomain, non-empty User-Agent)
+    """
+    ...
+
+# Could not authenticate to the Geometry Dash servers
+class AuthError(IHBSError):
+    """
+    Could not authenticate to the Geometry Dash servers
     """
     ...
